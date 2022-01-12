4a54430f52f6198b793f3f6758b496cc3e5975a4
genchanman <kingegenkikinge@gmail.com>
2022-01-12 16:05:20 +0900

    適切なコミットコメント

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+use App\Post;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+
+
+{
+   public function index(Post $post)
+{
+    return $post->get();
+}
+}
