dde84980194a1c75cc1978286985de536f32bd2a
genchanman <kingegenkikinge@gmail.com>
2022-01-25 20:24:57 +0900

    投稿画面の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,15 @@ public function store(PostRequest $request, Post $post)
     $post->fill($input["post"])->save();
     return redirect('/posts/' . $post->id);
 }
+public function edit(Post $post)
+{
+    return view('posts/edit')->with(['post' => $post]);
+}
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+}
 }
